2c540f51d4c6ebc14400b258ee02dea3aab62c1b
mzaini30 <mzaini30@users.noreply.github.com>
2020-08-26 14:28:22 +0000

    Update gitbook 2020-08-26 14:28:22

---
+++ vendor/auto-scroll-table.js
@@ -0,0 +1,14 @@
+require(['gitbook', 'jQuery'], (gitbook, $) => {
+	ubah = () => {
+		(function() {
+		    [].slice.call(document.querySelectorAll('table')).forEach(function(el){
+		        var wrapper = document.createElement('div');
+		        wrapper.className = 'table-area';
+		        el.parentNode.insertBefore(wrapper, el);
+		        el.parentNode.removeChild(el);
+		        wrapper.appendChild(el);
+		    });
+		})();
+	}
+	gitbook.events.bind('page.change', ubah)
+})
